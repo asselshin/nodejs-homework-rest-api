d1ddadefd3c3fc168619195fa33f8bf2b202b086
Assel Shingissova <asselshin@gmail.com>
2023-08-17 17:10:25 +0600

    authentification

--- controllers/contacts/getListContacts.js
+++ controllers/contacts/getListContacts.js
@@ -1,7 +1,55 @@
 const { Contact } = require("../../models/contacts");
+const paginate = require("mongoose-paginate");
+
+// пагинация
+// const getListContacts = async (req, res) => {
+//   const page = parseInt(req.query.page) || 1;
+//   const limit = parseInt(req.query.limit) || 5;
+
+//   const options = {
+//     page,
+//     limit,
+//     sort: { name: 1 },
+//   };
+
+//   const result = await Contact.paginate({}, options);
+
+//   res.json(result);
+// };
+
+// фильтрация по favorite
+// const getListContacts = async (req, res) => {
+//   const favorite = req.query.favorite;
+
+//   const filter = {};
+
+//   if (favorite !== undefined) {
+//     filter.favorite = favorite;
+//   }
+//   const result = await Contact.find(filter);
+
+//   res.json(result);
+// };
 
 const getListContacts = async (req, res) => {
-  const result = await Contact.find({});
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 3;
+  const favorite = req.query.favorite;
+
+  const filter = {};
+
+  const options = {
+    page,
+    limit,
+    sort: { name: 1 },
+  };
+
+  if (favorite !== undefined) {
+    filter.favorite = favorite;
+  }
+
+  const result = await Contact.paginate(filter, options);
+
   res.json(result);
 };
 
---
+++ routes/users/auth.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { validateBody, authenticate } = require("../../middlewares");
+const { ctrlWrapper } = require("../../helpers");
+const schemas = require("../../schemas/contacts");
+const ctrl = require("../../controllers/auth");
+
+const router = express.Router();
+
+router.post(
+  "/register",
+  validateBody(schemas.authSchema),
+  ctrlWrapper(ctrl.register)
+);
+
+router.post(
+  "/login",
+  validateBody(schemas.authSchema),
+  ctrlWrapper(ctrl.login)
+);
+
+router.get("/logout", authenticate, ctrlWrapper(ctrl.logout));
+
+router.get("/current", authenticate, ctrlWrapper(ctrl.getCurrent));
+
+router.patch(
+  "/",
+  authenticate,
+  validateBody(schemas.updateSubscription),
+  ctrlWrapper(ctrl.updateSubscription)
+);
+
+module.exports = router;
